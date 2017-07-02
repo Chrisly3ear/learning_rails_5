5c9776f5ec3eb4162639fb737d23c0a241b910eb
Chris <christoph@schmeisserweb.at>
2017-07-02 14:44:28 +0200

    Eigene Routen fuern Login-Prozess

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users, only: [:create]
+  get "register" => "users#new"
 
-  resource  :session
+  resource  :session, only: [:create]
+  get "login" => "sessions#new"
+  delete "logout" => "sessions#destroy"
+
+
+
 
   resources :posts do
     resources :comments
